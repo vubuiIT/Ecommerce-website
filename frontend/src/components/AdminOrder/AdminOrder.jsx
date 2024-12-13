7e0f28ab81fe37f59b6af936c7b43998b467f72c
Quang Vu <lyvu2804@gmail.com>
2024-12-13 20:05:52 +0700

    update AdminPage

--- frontend/src/components/AdminOrder/AdminOrder.jsx
+++ frontend/src/components/AdminOrder/AdminOrder.jsx
@@ -22,6 +22,16 @@ import { orderContant } from "../../contant";
 import PieChartComponent from "./PieChart";
 
 const OrderAdmin = () => {
+    const user = useSelector((state) => state?.user);
+
+    const getAllOrder = async () => {
+        const res = await OrderService.getAllOrder(user?.access_token);
+        return res;
+    };
+
+    const queryOrder = useQuery({ queryKey: ["orders"], queryFn: getAllOrder });
+    const { isLoading: isLoadingOrders, data: orders } = queryOrder;
+
     const getColumnSearchProps = (dataIndex) => ({
         filterDropdown: ({
             setSelectedKeys,
@@ -36,11 +46,13 @@ const OrderAdmin = () => {
                 onKeyDown={(e) => e.stopPropagation()}
             >
                 <InputComponent
+                    // ref={searchInput}
                     placeholder={`Search ${dataIndex}`}
                     value={selectedKeys[0]}
                     onChange={(e) =>
                         setSelectedKeys(e.target.value ? [e.target.value] : [])
                     }
+                    // onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
                     style={{
                         marginBottom: 8,
                         display: "block",
@@ -49,6 +61,7 @@ const OrderAdmin = () => {
                 <Space>
                     <Button
                         type="primary"
+                        // onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
                         icon={<SearchOutlined />}
                         size="small"
                         style={{
@@ -58,6 +71,7 @@ const OrderAdmin = () => {
                         Search
                     </Button>
                     <Button
+                        // onClick={() => clearFilters && handleReset(clearFilters)}
                         size="small"
                         style={{
                             width: 90,
@@ -82,8 +96,23 @@ const OrderAdmin = () => {
                 .includes(value.toLowerCase()),
         onFilterDropdownOpenChange: (visible) => {
             if (visible) {
+                // setTimeout(() => searchInput.current?.select(), 100);
             }
         },
+        // render: (text) =>
+        //   searchedColumn === dataIndex ? (
+        //     // <Highlighter
+        //     //   highlightStyle={{
+        //     //     backgroundColor: '#ffc069',
+        //     //     padding: 0,
+        //     //   }}
+        //     //   searchWords={[searchText]}
+        //     //   autoEscape
+        //     //   textToHighlight={text ? text.toString() : ''}
+        //     // />
+        //   ) : (
+        //     text
+        //   ),
     });
 
     const columns = [
@@ -131,12 +160,35 @@ const OrderAdmin = () => {
         },
     ];
 
+    const dataTable =
+        orders?.data?.length &&
+        orders?.data?.map((order) => {
+            console.log("usewr", order);
+            return {
+                ...order,
+                key: order._id,
+                userName: order?.shippingAddress?.fullName,
+                phone: order?.shippingAddress?.phone,
+                address: order?.shippingAddress?.address,
+                paymentMethod: orderContant.payment[order?.paymentMethod],
+                isPaid: order?.isPaid ? "TRUE" : "FALSE",
+                isDelivered: order?.isDelivered ? "TRUE" : "FALSE",
+                totalPrice: convertPrice(order?.totalPrice),
+            };
+        });
+
     return (
         <div>
             <WrapperHeader>Quản lý đơn hàng</WrapperHeader>
-
+            <div style={{ height: 200, width: 200 }}>
+                <PieChartComponent data={orders?.data} />
+            </div>
             <div style={{ marginTop: "20px" }}>
-                <TableComponent columns={columns} />
+                <TableComponent
+                    columns={columns}
+                    isLoading={isLoadingOrders}
+                    data={dataTable}
+                />
             </div>
         </div>
     );
