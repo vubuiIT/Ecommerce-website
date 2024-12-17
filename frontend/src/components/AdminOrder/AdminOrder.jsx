3d383acb16230e8a9d5c53faee98de37d06d3117
Quang Vu <lyvu2804@gmail.com>
2024-12-17 10:45:47 +0700

    fix feature search and filter in order table

--- frontend/src/components/AdminOrder/AdminOrder.jsx
+++ frontend/src/components/AdminOrder/AdminOrder.jsx
@@ -1,28 +1,22 @@
-import { Button, Form, Space } from "antd";
-import React from "react";
-import { WrapperHeader, WrapperUploadFile } from "./style";
+import { Button, Space } from "antd";
+import React, { useState, useRef } from "react";
+import { WrapperHeader } from "./style";
 import TableComponent from "../TableComponent/TableComponent";
 import InputComponent from "../InputComponent/InputComponent";
-import DrawerComponent from "../DrawerComponent/DrawerComponent";
-import Loading from "../LoadingComponent/Loading";
-import ModalComponent from "../ModalComponent/ModalComponent";
 import { convertPrice, getBase64 } from "../../utils";
-import { useEffect } from "react";
-import * as message from "../Message/Message";
-
 import * as OrderService from "../../services/OrderService";
 import { useQuery } from "@tanstack/react-query";
-import {
-    DeleteOutlined,
-    EditOutlined,
-    SearchOutlined,
-} from "@ant-design/icons";
+import { SearchOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
 import { orderContant } from "../../contant";
 import PieChartComponent from "./PieChart";
+import Highlighter from "react-highlight-words";
 
 const OrderAdmin = () => {
     const user = useSelector((state) => state?.user);
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+    const searchInput = useRef(null);
 
     const getAllOrder = async () => {
         const res = await OrderService.getAllOrder(user?.access_token);
@@ -31,6 +25,16 @@ const OrderAdmin = () => {
 
     const queryOrder = useQuery({ queryKey: ["orders"], queryFn: getAllOrder });
     const { isLoading: isLoadingOrders, data: orders } = queryOrder;
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText("");
+    };
 
     const getColumnSearchProps = (dataIndex) => ({
         filterDropdown: ({
@@ -46,13 +50,15 @@ const OrderAdmin = () => {
                 onKeyDown={(e) => e.stopPropagation()}
             >
                 <InputComponent
-                    // ref={searchInput}
+                    ref={searchInput}
                     placeholder={`Search ${dataIndex}`}
                     value={selectedKeys[0]}
                     onChange={(e) =>
                         setSelectedKeys(e.target.value ? [e.target.value] : [])
                     }
-                    // onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    onPressEnter={() =>
+                        handleSearch(selectedKeys, confirm, dataIndex)
+                    }
                     style={{
                         marginBottom: 8,
                         display: "block",
@@ -61,7 +67,9 @@ const OrderAdmin = () => {
                 <Space>
                     <Button
                         type="primary"
-                        // onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        onClick={() =>
+                            handleSearch(selectedKeys, confirm, dataIndex)
+                        }
                         icon={<SearchOutlined />}
                         size="small"
                         style={{
@@ -71,7 +79,9 @@ const OrderAdmin = () => {
                         Search
                     </Button>
                     <Button
-                        // onClick={() => clearFilters && handleReset(clearFilters)}
+                        onClick={() =>
+                            clearFilters && handleReset(clearFilters)
+                        }
                         size="small"
                         style={{
                             width: 90,
@@ -96,23 +106,23 @@ const OrderAdmin = () => {
                 .includes(value.toLowerCase()),
         onFilterDropdownOpenChange: (visible) => {
             if (visible) {
-                // setTimeout(() => searchInput.current?.select(), 100);
+                setTimeout(() => searchInput.current?.select(), 100);
             }
         },
-        // render: (text) =>
-        //   searchedColumn === dataIndex ? (
-        //     // <Highlighter
-        //     //   highlightStyle={{
-        //     //     backgroundColor: '#ffc069',
-        //     //     padding: 0,
-        //     //   }}
-        //     //   searchWords={[searchText]}
-        //     //   autoEscape
-        //     //   textToHighlight={text ? text.toString() : ''}
-        //     // />
-        //   ) : (
-        //     text
-        //   ),
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: "#ffc069",
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ""}
+                />
+            ) : (
+                text
+            ),
     });
 
     const columns = [
@@ -137,33 +147,77 @@ const OrderAdmin = () => {
         {
             title: "Paided",
             dataIndex: "isPaid",
-            sorter: (a, b) => a.isPaid.length - b.isPaid.length,
-            ...getColumnSearchProps("isPaid"),
+            filters: [
+                {
+                    text: "TRUE",
+                    value: "TRUE",
+                },
+                {
+                    text: "FALSE",
+                    value: "FALSE",
+                },
+            ],
+            onFilter: (value, record) => {
+                if (value === "TRUE") {
+                    return record.isPaid === "TRUE";
+                }
+                return record.isPaid === "FALSE";
+            },
         },
         {
             title: "Shipped",
             dataIndex: "isDelivered",
-            sorter: (a, b) => a.isDelivered.length - b.isDelivered.length,
-            ...getColumnSearchProps("isDelivered"),
+            filters: [
+                {
+                    text: "TRUE",
+                    value: "TRUE",
+                },
+                {
+                    text: "FALSE",
+                    value: "FALSE",
+                },
+            ],
+            onFilter: (value, record) => {
+                if (value === "TRUE") {
+                    return record.isDelivered === "TRUE";
+                }
+                return record.isDelivered === "FALSE";
+            },
         },
         {
             title: "Payment method",
             dataIndex: "paymentMethod",
-            sorter: (a, b) => a.paymentMethod.length - b.paymentMethod.length,
-            ...getColumnSearchProps("paymentMethod"),
+
+            filters: [
+                {
+                    text: "Thanh toán tiền mặt khi nhận hàng",
+                    value: "Thanh toán tiền mặt khi nhận hàng",
+                },
+                {
+                    text: "Thanh toán bằng paypal",
+                    value: "Thanh toán bằng paypal",
+                },
+            ],
+            onFilter: (value, record) => {
+                if (value === "Thanh toán tiền mặt khi nhận hàng") {
+                    return (
+                        record.paymentMethod ===
+                        "Thanh toán tiền mặt khi nhận hàng"
+                    );
+                }
+                return record.paymentMethod === "Thanh toán bằng paypal";
+            },
         },
         {
             title: "Total price",
             dataIndex: "totalPrice",
             sorter: (a, b) => a.totalPrice.length - b.totalPrice.length,
-            ...getColumnSearchProps("totalPrice"),
         },
     ];
 
     const dataTable =
         orders?.data?.length &&
         orders?.data?.map((order) => {
-            console.log("usewr", order);
             return {
                 ...order,
                 key: order._id,
